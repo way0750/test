0f35d395d31e1cb9d9269321d2c6a95eb0adfeda
way0750 <bruce0750@gmail.com>
2015-08-27 16:01:07 -0700

    removed useless lines

--- js/main.js
+++ js/main.js
@@ -22,13 +22,7 @@ var checkNumbers = function(event) {
   console.log("results: ", results);
 
   // EDIT #2: add message to section#message based on whether or not user has guessed correctly
-  
-
-$('#message').text(
-  'seriously what\'s going on???'
-  )
-
-
+ 
   $("#show-num").text(
     "The numbers were " + lotto1 +  " and " + lotto2 + ". " +
     inputs[0] + " was " + results[inputs[0]] + "; " +
